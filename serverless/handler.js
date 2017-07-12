f88fe1ecaca11c83dfa203fe51ec47cefeb4955a
lakshman <lakshman@fx24106.local>
2017-07-12 18:20:01 +0900

    initial commit

--- serverless/handler.js
+++ serverless/handler.js
@@ -1,44 +1,20 @@
-const jwt = require('jsonwebtoken');
+/* eslint-disable global-require, import/first, no-unused-expressions, no-console */
+if (!global._babelPolyfill) require('babel-polyfill');
 
-const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
-const AUTH0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET;
+import lib from './lib';
 
-// Policy helper function
-const generatePolicy = (principalId, effect, resource) => {
-  const authResponse = {};
-  authResponse.principalId = principalId;
-  if (effect && resource) {
-    const policyDocument = {};
-    policyDocument.Version = '2012-10-17';
-    policyDocument.Statement = [];
-    const statementOne = {};
-    statementOne.Action = 'execute-api:Invoke';
-    statementOne.Effect = effect;
-    statementOne.Resource = resource;
-    policyDocument.Statement[0] = statementOne;
-    authResponse.policyDocument = policyDocument;
-  }
-  return authResponse;
-};
-
-// Reusable Authorizer function, set on `authorizer` field in serverless.yml
-module.exports.auth = (event, context, cb) => {
-  if (event.authorizationToken) {
-    // remove "bearer " from token
-    const token = event.authorizationToken.substring(7);
-    const options = {
-      audience: AUTH0_CLIENT_ID,
-    };
-    jwt.verify(token, AUTH0_CLIENT_SECRET, options, (err, decoded) => {
-      if (err) {
-        cb('Unauthorized');
-      } else {
-        cb(null, generatePolicy(decoded.sub, 'Allow', event.methodArn));
+module.exports.auth = (event, context) => {
+  // lib contains the logic for authenticating the access_token
+  lib.authenticate(event, (err, data) => {
+    if (err) {
+      if (!err) {
+        context.fail("unhandled error!!");
       }
-    });
-  } else {
-    cb('Unauthorized');
-  }
+      context.fail("Unauthorized!!");
+    } else {
+      context.succeed(data);
+    }
+  });
 };
 
 // Public API
