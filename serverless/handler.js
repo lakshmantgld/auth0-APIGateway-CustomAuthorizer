d48b66d99b8f62d1e60afb984c44fbf637da112f
lakshman <lakshman@fx24106.local>
2017-07-13 10:41:07 +0900

    scaffolding

--- serverless/handler.js
+++ serverless/handler.js
@@ -1,28 +1,91 @@
 /* eslint-disable global-require, import/first, no-unused-expressions, no-console */
 if (!global._babelPolyfill) require('babel-polyfill');
 
-import lib from './lib';
+import jwksClient from 'jwks-rsa';
+import jwt from 'jsonwebtoken';
 
-module.exports.auth = (event, context) => {
-  // lib contains the logic for authenticating the access_token
-  lib.authenticate(event, (err, data) => {
-    if (err) {
-      if (!err) {
-        context.fail("unhandled error!!");
+// IAM policy generator that is returned at the end of custom Authorizer.
+const getPolicyDocument = (effect, resource) => {
+  let policyDocument = {};
+
+  policyDocument.Version = '2012-10-17';
+  policyDocument.Statement = [];
+
+  let statementOne = {};
+  statementOne.Action = 'execute-api:Invoke';
+  statementOne.Effect = effect;
+  statementOne.Resource = resource;
+  policyDocument.Statement[0] = statementOne;
+
+  return policyDocument;
+};
+
+// Extract and return the Bearer Token from the Lambda event parameters
+const getToken = (event) => {
+  let token;
+
+  if (!event.type || event.type !== 'TOKEN') {
+    throw new Error("Expected 'event.type' parameter to have value TOKEN");
+  }
+
+  let tokenString = event.authorizationToken;
+  if (!tokenString) {
+    throw new Error("Expected 'event.authorizationToken' parametere to be set");
+  }
+
+  let match = tokenString.match(/^Bearer (.*)$/);
+  if (!match || match.length < 2) {
+    throw new Error("Invalid Authorization token -" + tokenString + " does not match the bearer");
+  }
+  return match[1];
+
+};
+
+module.exports.auth = (event, context, callback) => {
+  console.log(event);
+
+  // jwt.decode may throw error in case of validating wrong/expired access_token
+  try {
+    let token, client, decoded, kid;
+
+    token = getToken(event);
+    client = jwksClient({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 10,
+      jwksUri: process.env.JWKS_URI
+    });
+
+    decoded = jwt.decode(token, {complete: true});
+    kid = decoded.header.kid;
+
+    client.getSigningKey(kid, (err, key) => {
+      if (err) {
+        throw new Error(err);
+      } else {
+        let signingKey = key.publicKey || key.rsaPublicKey;
+        jwt.verify(token, signingKey, {audience: process.env.AUDIENCE, issuer: process.env.TOKEN_ISSUER}, (err, decoded) => {
+          if (err) {
+            throw new Error(err);
+          } else {
+            callback(null, {principalId: decoded.sub, policyDocument: getPolicyDocument('Allow', event.methodArn), context: {scope: decoded.scope}});
+          }
+        });
       }
-      context.fail("Unauthorized!!");
-    } else {
-      context.succeed(data);
-    }
-  });
+    });
+
+  } catch (err) {
+    callback(null, {policyDocument: getPolicyDocument('Deny', event.methodArn)});
+  }
+
 };
 
 // Public API
-module.exports.public = (event, context, cb) => {
-  cb(null, { message: 'Welcome to our Public API!' });
+module.exports.public = (event, context, callback) => {
+  callback(null, { message: 'Welcome to our Public API!' });
 };
 
 // Private API
-module.exports.private = (event, context, cb) => {
-  cb(null, { message: 'Only logged in users can see this' });
+module.exports.private = (event, context, callback) => {
+  callback(null, { message: 'Only logged in users can see this' });
 };
